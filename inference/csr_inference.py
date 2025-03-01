adaafb965d5eba4b5155d5ccf4608fd7003432c5
Tiansheng Wen <neilwen987@gmail.com>
2025-03-01 18:04:22 +0800

    Final Version

--- inference/csr_inference.py
+++ inference/csr_inference.py
@@ -29,8 +29,7 @@ parser.add_argument('--workers', type=int, default=16, help='num workers for dat
 parser.add_argument('--batch_size', default=256,type=int,help='batch size')
 parser.add_argument('--embed_save_path', default='../retrieval/pretrained_emb', help='path to save database and query arrays for retrieval', type=str)
 parser.add_argument('--model_name', default='resnet50d.ra4_e3600_r224_in1k',help='timm model name')
-parser.add_argument('--backbone_ckpt', default='/mnt/b6358dbf-93d5-42d7-adee-9793f027e744/WTS/Matryoshka_NCL/'
-                                               'examples-main/imagenet/resnet_sota_pretrain/pytorch_model.bin',help='timm model ckpt')
+
 
 
 # CSR args
@@ -39,12 +38,11 @@ parser.add_argument('--auxk', default=512, type=int,help='the number of auxk')
 parser.add_argument('--auxk_coef', default=1/32, type=float,help='auxk_coef')
 parser.add_argument('--dead_threshold', default=10, type=int,help='dead_threshold')
 parser.add_argument('--hidden-size', default=None, type=int,help='the size of hidden layer')
-parser.add_argument('--csr-ckpt', default='/mnt/b6358dbf-93d5-42d7-adee-9793f027e744/WTS/Matryoshka_NCL/examples-main/'
-                                          'pz_sae_multi_topk/ckpt/zlcq1r9g_best_cl_full/checkpoint_9.pth',type=str,help = 'ckpt for CSR model')
+parser.add_argument('--csr-ckpt', default=None,type=str,help = 'ckpt for CSR model')
 
 args = parser.parse_args()
 
-sota_backbone = timm.create_model(args.model_name, pretrained=False, num_classes=1000,)
+sota_backbone = timm.create_model(args.model_name, pretrained=True, num_classes=1000,)
 if args.hidden_size is None:
     args.hidden_size = sota_backbone.fc.in_features * 4
 model = CSR(n_latents=args.hidden_size, topk=args.topk, auxk=args.auxk, dead_threshold=args.dead_threshold,
